36b6232707a0eeeb1c61f90dc57eb24ad8f05185
atharvmenon <72930077+atharvmenon@users.noreply.github.com>
2021-01-28 19:47:49 +0530

    Add files via upload

---
+++ class 34/asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,61 @@
+var ball;
+var database;
+var position;
+
+function setup(){
+    createCanvas(500,500);
+
+    database = firebase.database();
+
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var ballPosition = database.ref("ball/position");
+    ballPosition.on("value", readPosition);
+    
+}
+
+function draw(){ 
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+       writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+       writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+
+    database.ref("ball/position").set({
+        x: position.x+x,
+        y: position.y+y
+    });
+
+    //ball.x = ball.x + x;
+    //ball.y = ball.y + y;
+}
+
+function readPosition(data){
+    position = data.val();
+
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+
+
+//remote common database
+//firebase console - Google
+//Real Time Database
+
+//.ref() -  to refer/go to the location of the database value we care about.
+//.on() -  a listener/reading function which keeps listening/reading to the changes in the database.
+//.set() - to set/write/update the value in the database
